91747166d9c3fe5790f7784a4665ccb17cc7f76f
X <bernardtech.co1@gmail.com>
2026-04-21 20:28:29 +0100

    InitialCommit

---
+++ practice.js
@@ -0,0 +1,16 @@
+class name{
+    constructor(name, age){
+        this.name = name
+        this.age = age
+
+    };
+    displayFunction(){
+
+        return this.name + this.age
+    };
+
+}
+
+let obj = new name("Bernard", 18);
+
+console.log(obj.displayFunction());
